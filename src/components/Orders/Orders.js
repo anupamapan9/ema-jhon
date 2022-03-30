c800cf484b6e3d46d966ff98fecc3812b9d9967a
anupamapan9 <anupamapan0905@gmail.com>
2022-03-31 00:08:38 +0600

    cart hook added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products)
     return (
         <div>
-            <h1>This is orders {products.length}</h1>
+            <h1>This is orders {cart.length}</h1>
         </div>
     );
 };
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,13 +1,12 @@
-const { useState, useEffect } = require("react")
+import { useEffect, useState } from "react"
 
 const useProducts = () => {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([])
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-
     }, [])
-    return [products, setProducts];
+    return [products, setProducts]
 }
-export default useProducts;
+export default useProducts
